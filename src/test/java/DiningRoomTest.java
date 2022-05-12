16b8827312e2fc41227133751de86f04692bfd89
CLg003 <clairemjlaing@gmail.com>
2022-05-12 13:28:15 +0100

    DiningRoom class tests passed.

---
+++ src/test/java/DiningRoomTest.java
@@ -0,0 +1,20 @@
+import hotel.rooms.DiningRoom;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DiningRoomTest {
+
+    DiningRoom diningRoom;
+
+    @Before
+    public void before(){
+        diningRoom = new DiningRoom(30, "Dining Room");
+    }
+
+    @Test
+    public void diningRoomHasName(){
+        assertEquals("Dining Room", diningRoom.getName());
+    }
+}
