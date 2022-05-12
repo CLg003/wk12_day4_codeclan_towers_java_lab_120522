b8acb1581b362789d2e0a13905f81766c4e62427
CLg003 <clairemjlaing@gmail.com>
2022-05-12 10:35:29 +0100

    RoomType enum created.

---
+++ src/main/java/rooms/RoomType.java
@@ -0,0 +1,18 @@
+package rooms;
+
+public enum RoomType {
+    SINGLE(1),
+    DOUBLE(2),
+    FAMILY(4),
+    SUITE(2);
+
+    private final int value;
+
+    RoomType(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+}
