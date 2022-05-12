cd4472bdfcb321f1603c28aed7d03d863ab0ab82
CLg003 <clairemjlaing@gmail.com>
2022-05-12 11:14:31 +0100

    ConferenceRoom tests all passed.

--- src/main/java/hotel/rooms/ConferenceRoom.java
+++ src/main/java/hotel/rooms/ConferenceRoom.java
@@ -1,4 +1,21 @@
 package hotel.rooms;
 
-public class ConferenceRoom {
+public class ConferenceRoom extends Room{
+
+    private String name;
+    private Boolean audioVisual;
+
+    public ConferenceRoom(int capacity, String name, Boolean audioVisual) {
+        super(capacity);
+        this.name = name;
+        this.audioVisual = audioVisual;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Boolean getAudioVisual() {
+        return audioVisual;
+    }
 }
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,2 +1,35 @@
+import hotel.rooms.ConferenceRoom;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceRoomTest {
+
+    ConferenceRoom conferenceRoom;
+
+    @Before
+    public void before(){
+        conferenceRoom = new ConferenceRoom(10, "Boardroom", true);
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(10, conferenceRoom.getCapacity());
+    }
+
+    @Test
+    public void startWithZeroGuests(){
+        assertEquals(0, conferenceRoom.getNumberOfGuests());
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Boardroom", conferenceRoom.getName());
+    }
+
+    @Test
+    public void hasAudioVisual(){
+        assertEquals(true, conferenceRoom.getAudioVisual());
+    }
 }
